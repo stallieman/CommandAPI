397bf5600fbaf638782275cab738644af936ca2d
stallieman <marco.stalman@home.nl>
2021-11-29 15:20:11 +0100

    small change

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,7 +1,10 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using CommandAPI.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace CommandAPI.Controllers
 {
@@ -10,13 +13,85 @@ namespace CommandAPI.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly CommandContext _context;
-        public CommandsController(CommandContext context) => _context = context;
+        private IWebHostEnvironment _hostEnv;
+
+        public CommandsController(CommandContext context, IWebHostEnvironment hostEnv)
+        {
+            _context = context;
+            _hostEnv = hostEnv;
+        }
+
 
         //GET:      api/commands
         [HttpGet]
-        public IEnumerable<Command> GetCommandItems()
-        { 
-            return _context.CommandItems; 
+        public ActionResult<IEnumerable<Command>> GetCommandItems()
+        {
+            //We'll remove after moving to production
+            if(Response != null)
+                Response.Headers.Add("Environment", _hostEnv.EnvironmentName);
+
+            return _context.CommandItems;
         }
+
+        //GET:      api/commands/Id
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
+            
+            if(commandItem == null)
+                return NotFound();
+            
+            return commandItem; 
+        }
+        
+        
+        //POST:     api/commands
+        [HttpPost]
+        public ActionResult<Command> PostCommandItem(Command command)
+        {
+            _context.CommandItems.Add(command);
+            
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction("GetCommandItem", new Command{Id = command.Id}, command);
+        }
+
+         //PUT:     api/commands/id
+         [HttpPut("{id}")]
+         public ActionResult PutCommandItem(int id, Command command)
+         {
+             if (id != command.Id)
+             {
+                 return BadRequest();
+             }
+
+             _context.Entry(command).State = EntityState.Modified; 
+             _context.SaveChanges();
+
+             return NoContent();
+         }
+
+         //DELETE:  api/commands/id
+         [HttpDelete("{id}")]
+         public ActionResult<Command> DeleteCommandItem(int id)
+         {
+             var commandItem = _context.CommandItems.Find(id);
+
+             if (commandItem == null)
+                return NotFound();
+
+            _context.CommandItems.Remove(commandItem);
+            _context.SaveChanges();
+
+            return commandItem;
+         }
     }
 }
