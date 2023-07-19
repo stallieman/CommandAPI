b7d723340fe310d1b567ebd706dfb7220f449ea0
“stallieman” <stallieman@protonmail.com>
2023-07-19 15:48:05 +0200

    Command API van 3.1 naar 6

---
+++ Program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using CommandAPI.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using AutoMapper;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
+
+var services = builder.Services;
+
+var npgsqlBuilder = new NpgsqlConnectionStringBuilder();
+npgsqlBuilder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+npgsqlBuilder.Username = Configuration["UserID"];
+npgsqlBuilder.Password = Configuration["Password"];
+
+services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(npgsqlBuilder.ConnectionString));
+
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt => 
+{
+    opt.Audience = Configuration["ResourceID"];
+    opt.Authority = $"{Configuration["Instance"]}{Configuration["TenantId"]}";
+});
+
+services.AddControllers().AddNewtonsoftJson(s => 
+{
+    s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+});
+
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
